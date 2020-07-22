24b7995a921886d8e586492a328ad82de2d25bbe
Vijay S <37852613+vijay043077@users.noreply.github.com>
2020-07-21 19:00:22 -0500

    new commit

--- Starter_Code/UFO-level-1/static/app.js
+++ Starter_Code/UFO-level-1/static/app.js
@@ -1,4 +1,40 @@
 // from data.js
 var tableData = data;
 
-// YOUR CODE HERE!
+// Select the button
+var button = d3.select("#button");
+
+// Select the form
+var form = d3.select("#form");
+
+button.on("click", runEnter);
+
+form.on("submit", runEnter);
+
+function runEnter() {
+    d3.event.preventDefault();
+
+    var inputElement = d3.select("#datetime");
+
+    var inputValue = inputElement.property("value");
+
+    console.log(inputValue);
+    console.log(tableData);
+
+    var filteredData = tableData.filter(data => data.datetime === inputValue);
+
+    console.log(filteredData);
+
+    var tbody = d3.select("tbody");
+    console.log(data);
+
+    data.forEach((alienSighting) => {
+        var row = tbody.append("tr");
+        Object.values(alienSighting).forEach((value) => {
+            var cell = row.append("td");
+            cell.text(value);
+        });
+    });
+
+}
+runEnter();
